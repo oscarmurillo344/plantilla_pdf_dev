9303483f2890bd037a52c92a75d00e519ad54017
unknown <oscarmurillo344@gmail.com>
2021-02-11 13:18:53 -0500

    version 1

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const app=express();
 
-app.use(express.static(__dirname+'/dist/PlantillaPDF'));
+app.use(express.static(__dirname+'/dist/PlantillaPdf'));
 app.get('/*',function (req,res) {
-    res.sendFile(path.join(__dirname+'/dist/PlantillaPDF/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/PlantillaPdf/index.html'));
 })
 app.listen(process.env.PORT || 8080);
